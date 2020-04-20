89f4c948da7ca4d6638e9e49511ddea0aa0ab6fa
Charith Abeygunawardhana <charith9422@gmail.com>
2020-04-20 09:42:02 +0530

    Overflow UI bug fixed

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatGridListModule } from "@angular/material/grid-list";
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatIconModule} from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 const materialComponents = [
-  MatButtonModule, 
+  MatButtonModule,
   MatGridListModule,
   MatCardModule,
   MatDividerModule,
-  MatIconModule
+  MatIconModule,
+  MatExpansionModule,
 ];
 
 @NgModule({
   declarations: [],
   imports: [materialComponents],
-  exports: [materialComponents]
+  exports: [materialComponents],
 })
-export class MaterialModule {}
+export class MaterialModule { }
